8844245a0ba91ec6bcae835bf8b4555065ea875e
ankitsharma6652 <ankitcoolji@gmail.com>
2022-02-27 17:14:22 +0530

    Stage 01 updated

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -3,46 +3,50 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
-import tensorflow as tf
-import os
-from tqdm import tqdm
-import urllib.request as req
+from src.utils.common import read_yaml, create_directories, unzip_file
+from src.utils.data_mgmt import validate_image
 import random
+import urllib.request as req
 
-
-STAGE = "GET_DATA" ## <<< change stage name
+STAGE = "GET_DATA"  ## <<< change stage name
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'),
     level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
-    )
+)
 
 
 def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    # params = read_yaml(params_path)
-    URL=config['data']['source_url']
-    local_dir=config['data']['local_dir']
+    URL = config["data"]["source_url"]
+    local_dir = config["data"]["local_dir"]
     create_directories([local_dir])
-    data_file=config['data']['data_file']
-    data_file_path=os.path.join(local_dir,data_file)
+
+    data_file = config["data"]["data_file"]
+    data_file_path = os.path.join(local_dir, data_file)
 
     if not os.path.isfile(data_file_path):
-        logging.info("Downloading Started")
+        logging.info("downloading started...")
         filename, headers = req.urlretrieve(URL, data_file_path)
         logging.info(f"filename:{filename} created with info \n{headers}")
     else:
-        logging.info(f"File {data_file} already present")
+        logging.info(f"filename:{data_file} already present")
+
+    # Unzip ops
+    unzip_data_dir = config["data"]["unzip_data_dir"]
+    create_directories([unzip_data_dir])
+    unzip_file(source=data_file_path, dest=unzip_data_dir)
+
+    # validating data
+    validate_image(config)
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
