4aa34a50b154e565e583efd6d9eaef1128e3aa1d
ankitsharma6652 <ankitcoolji@gmail.com>
2022-02-27 18:29:19 +0530

    stage 04 train updated

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ def main():
         mlflow.run(".", "get_data", use_conda=False)
         # mlflow.run(".", "get_data", parameters={}, use_conda=False)
         mlflow.run(".", "base_model_creation", use_conda=False)
+        mlflow.run(".", "training", use_conda=False)
 
 if __name__ == '__main__':
     try:
@@ -33,5 +34,4 @@ if __name__ == '__main__':
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
-        print(e)
         raise e
