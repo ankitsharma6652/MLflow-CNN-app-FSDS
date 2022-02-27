b282eb2fd2703916b5d89ec70af68c32a2d62e47
ankitsharma6652 <ankitcoolji@gmail.com>
2022-02-27 17:15:26 +0530

    Added bash file

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,6 @@
-conda create --prefix ./env python=3.7 -y
-source activate ./env
-pip install -r requirements.txt
-conda env export > conda.yaml
+#conda create --prefix ./env python=3.7 -y
+#source activate ./env
+#pip install -r requirements.txt
+#conda env export > conda.yaml
+
+#conda env list
